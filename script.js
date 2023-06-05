e82375e86c910ef480faadae018d94ccde560d46
emikepull <83540337+emike05@users.noreply.github.com>
2023-06-05 06:56:02 +0000

    Fixes to layout

--- script.js
+++ script.js
@@ -19,16 +19,45 @@
 //   }, 3000);
 // });
 
+// document.getElementById("orderForm").addEventListener("submit", function(event) {
+//   event.preventDefault();
+
+//   // Get form values
+//   var name = document.getElementById("name").value;
+//   var email = document.getElementById("email").value;
+//   var address = document.getElementById("address").value;
+
+//   // Display order processing message
+//   var orderBtn = document.getElementById("btn2");
+//   orderBtn.disabled = true;
+//   orderBtn.innerText = "Order in progress...";
+
+//   // Simulate order processing (replace with your actual order processing code)
+//   setTimeout(function() {
+//     // Display order confirmation
+//     btn2.innerText = "Order Complete!";
+//     alert("Thank you for your order, " + name + "! Your order is being prepared and will be delivered to " + address + " soon.");
+//     // Reset the form
+//     document.getElementById("orderForm").reset();
+//     orderBtn.disabled = false;
+//     orderBtn.innerText = "Order Now";
+//   }, 5000);
+// });
+
 document.getElementById("orderForm").addEventListener("submit", function(event) {
   event.preventDefault();
 
   // Get form values
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var address = document.getElementById("address").value;
+  var nameInput = document.getElementById("name");
+  var emailInput = document.getElementById("email");
+  var addressInput = document.getElementById("address");
+  var orderBtn = document.getElementById("btn2");
+
+  var name = nameInput.value;
+  var email = emailInput.value;
+  var address = addressInput.value;
 
   // Display order processing message
-  var orderBtn = document.getElementById("orderBtn");
   orderBtn.disabled = true;
   orderBtn.innerText = "Order in progress...";
 
@@ -37,12 +66,19 @@ document.getElementById("orderForm").addEventListener("submit", function(event) 
     // Display order confirmation
     orderBtn.innerText = "Order Complete!";
     alert("Thank you for your order, " + name + "! Your order is being prepared and will be delivered to " + address + " soon.");
+    
     // Reset the form
     document.getElementById("orderForm").reset();
     orderBtn.disabled = false;
     orderBtn.innerText = "Order Now";
-  }, 3000);
+
+    // Clear input values
+    nameInput.value = "";
+    emailInput.value = "";
+    addressInput.value = "";
+  }, 5000);
 });
+
 
 
 
